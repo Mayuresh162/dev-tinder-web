fe424d1b470807290d82ea3f08cbf6c9a700205d
Mayuresh Bhagat <mayureshbhagat0@gmail.com>
2025-07-27 11:35:56 +0530

    Added frontend basic boilerplate
    Added daisyui and tailwindcss
    Added navbar
    Added react router and basic routing
    Added login

---
+++ src/components/Login.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import axios from "axios";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-20">
+      <div className="card bg-primary text-primary-content w-96">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <fieldset className="fieldset border-base-300 rounded-box">
+            <label className="label">Email ID</label>
+            <input
+              type="email"
+              className="input w-full"
+              value={emailId}
+              onChange={(e) => setEmailId(e.target.value)}
+            />
+
+            <label className="label">Password</label>
+            <input
+              type="password"
+              className="input w-full"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button className="btn btn-neutral mt-4" onClick={handleLogin}>
+              Login
+            </button>
+          </fieldset>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
